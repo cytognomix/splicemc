87e2c24528d30010aa03ea2803916c39d57a8f29
cytognomix <Ben.Shirley@cytognomix.com>
2014-10-27 19:52:14 +0000

    Initial commit

--- templates/calc_footer.php
+++ templates/calc_footer.php
@@ -3,7 +3,7 @@
 		<span style="line-height:60px;">
 			<center>
 				<p style="margin:0;" class="text-muted">
-					<a style="text-decoration:none;color:#777777;" href="http://www.cytognomix.com">&copy; 2014 Cytognomix Inc. &mdash;
+					<a style="text-decoration:none;color:#777777;" href="http://www.cytognomix.com">&copy; 2014 Cytognomix Inc.</a> &mdash;
 					Designed and implemented by Shannon Brown, Ben Shirley, Natasha Caminsky, and Peter Rogan.	
 				</p>
       			</center>
